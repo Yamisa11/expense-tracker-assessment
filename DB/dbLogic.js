23b10c39d776e25d62a9be67e7af2f6277d0a6dd
Yamisa11 <majijayamisa@gmail.com>
2023-11-29 11:31:08 +0200

    initial commit

---
+++ DB/dbLogic.js
@@ -0,0 +1,39 @@
+export default function tracker(database) {
+
+
+  async function insertExpense(expense, amount, total, categoryId) {
+    const query = `
+        INSERT INTO expense (expense, amount, total, category_id)
+        VALUES ($1, $2, $3, $4)
+    `;
+    await database.any(query, [expense, amount, total,categoryId]);
+  }
+
+  async function getAll(){
+   const results = await database.any('SELECT * FROM expense')
+   return results
+  }
+
+  async function expenseCategory(categoryId){
+    let results = await database.any('SELECT * FROM expense WHERE category_id = $1', [categoryId])
+    return results
+  }
+
+  async function deleteExp(expenseId){
+    await database.none('DELETE FROM expense WHERE id = $1', [expenseId])
+  }
+
+  async function cateTotals(){
+    let results = await database.any('SELECT total FROM expense;')
+    return results
+  }
+
+  return {
+    insertExpense,
+    getAll,
+    expenseCategory,
+    deleteExp,
+    cateTotals
+  };
+
+}
---
+++ expense-tracker.js
@@ -0,0 +1,50 @@
+export default function expense(database){
+
+    async function addExpense(categoryid, amount,expense){
+        let total = 0
+
+        if (categoryid == 1){
+            total = amount * 4
+        }
+        if (categoryid == 2 || categoryid == 5){
+            total = amount 
+        }
+        if (categoryid == 3){
+            total = (amount * 5)*4
+        }
+        if (categoryid == 4){
+            total = (amount * 2)*4
+        }
+        if (categoryid == 6){
+            total = amount * 30
+        }
+
+        await database.insertExpense(expense, amount, total,categoryid)
+    }
+
+    async function allExpenses(){
+      let results=  await database.getAll()
+      return results
+    }
+
+    async function expenseForCategory(categoryid){
+
+    }
+
+    async function deleteExpense(expenseid){
+
+    }
+
+    async function categoryTotals(){
+
+
+    }
+
+    return{
+        addExpense,
+        allExpenses,
+        expenseForCategory,
+        deleteExpense,
+        categoryTotals
+    }
+}
