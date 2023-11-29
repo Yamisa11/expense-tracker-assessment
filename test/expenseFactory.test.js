d6136f5da3e6f9beac27728067ec8f1e1260ac7f
Yamisa11 <majijayamisa@gmail.com>
2023-11-29 16:05:25 +0200

    update

--- test/expenseFactory.test.js
+++ test/expenseFactory.test.js
@@ -13,19 +13,56 @@ describe('Expense Tracker factory Function tests', () => {
         await expenseFunction.reset()
     })
 
+    it('should be ale to get all the expenses in the database'), async () => {
+       let results = await expenseFunction.allExpenses()
+
+       await expenseFunction.addExpense(1, 30, "airtime");
+       await expenseFunction.addExpense(6, 40, "lunch");
+
+       assert.deepStrictEqual(results, [{  id: 1,
+        expense: 'airtime',
+        amount: 150,
+        total: 600,
+        category_id: 1},
+        {  id: 2,
+            expense: 'lunch',
+            amount: 50,
+            total: 1500,
+            category_id: 6
+        }])
+       
+    }
+
     it('should be able to insert an expense to table expense and retrieve it', async () => {
         await expenseFunction.addExpense(2, 150, "brunch");
     
         const result = await expenseFunction.getAll();
         assert.strictEqual(result[0], {
-            id: 2,
+            id: 1,
             expense: 'brunch',
             amount: 150,
-            total: '1500',
-            category_id: 5
+            total: 150,
+            category_id: 2
           });
         
       });
 
+      it("should be able to delete an expense from the table expense using the expense id", async () => {
+        await expenseFunction.addExpense(2, 150, "brunch");
 
+        await expenseFunction.deleteExpense(1)
+       let results = await expenseFunction.allExpenses()
+
+        assert.strictEqual(results, [])
+      })
+
+      it("should be able to add all the totals of the expenses and return a grand total", async () => {
+
+       await expenseFunction.addExpense(1, 30, "airtime");
+       await expenseFunction.addExpense(6, 40, "lunch");
+
+       let results = expenseFunction.categoryTotals()
+
+       assert.deepStrictEqual(results, 2100)
+      })
 })
